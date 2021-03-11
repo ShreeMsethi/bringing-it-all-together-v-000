755b6ff6dc8088e5aab8011ccfb74abaae464199
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-11 23:35:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,17 +73,17 @@
 #     end.first
 #   end
   
-#   def self.find_or_create_by(name:, breed:)
-#     dog = DB[:conn].execute("SELECT * FROM dogs 
-#     WHERE name = ? AND breed = ?", name, breed)
-#     if !dog.empty?
-#       dog_data = dog[0]
-#       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
-#     else
-#       dog = self.create(name: name, breed: breed)
-#     end
-#     dog
-#   end
+  # def self.find_or_create_by(name:, breed:)
+  #   dog = DB[:conn].execute("SELECT * FROM dogs 
+  #   WHERE name = ? AND breed = ?", name, breed)
+  #   if !dog.empty?
+  #     dog_data = dog[0]
+  #     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+  #   else
+  #     dog = self.create(name: name, breed: breed)
+  #   end
+  #   dog
+  # end
   
 #   def self.find_by_name(name)
 #     sql = <<-SQL 
@@ -170,7 +170,7 @@ class Dog
   end 
   
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?")
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
@@ -179,7 +179,18 @@ class Dog
     end 
     dog
   end  
-
+  
+  #   def self.find_or_create_by(name:, breed:)
+  #   dog = DB[:conn].execute("SELECT * FROM dogs 
+  #   WHERE name = ? AND breed = ?", name, breed)
+  #   if !dog.empty?
+  #     dog_data = dog[0]
+  #     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+  #   else
+  #     dog = self.create(name: name, breed: breed)
+  #   end
+  #   dog
+  # end
 end
 
 
